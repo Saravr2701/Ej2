236c5aed9ff9f182e1f9c02f51580752cbf47f6a
Iris <ichocantrave@safareyes.es>
2026-03-09 10:39:46 +0100

    Penne

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,10 @@ public class Main {
         for (int i=0; i<10; i++) {
             System.out.println("Holaaaa "+i);
         }
+<<<<<<< HEAD
         //me gustas Sara, puedo ser tu novia?
+=======
+        System.out.pritnln("Programita finalizado siuuuuuu");
+>>>>>>> 7dd31ced6447fdf63d2390619d108dcde0f229cd
     }
 }
